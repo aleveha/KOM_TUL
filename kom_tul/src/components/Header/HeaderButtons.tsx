c7556ec11f98a95252a93cf66c83d000eb3aac43
Nikita Tashchilin <tashchilin@proconom.cz>
2020-09-27 16:37:39 +0000

    Updated HeaderButtons.tsx

--- kom_tul/src/components/Header/HeaderButtons.tsx
+++ kom_tul/src/components/Header/HeaderButtons.tsx
@@ -1,33 +1,42 @@
 import * as React from 'react';
+import {Link} from 'react-router-dom';
+import {useContext} from "react";
+import PathContext from "../../Context/PathContext";
+import AppContentContext from "../../Context/AppContentContext";
 
 interface IProps {
-    changePathWay: (value: Array<string>) => void,
-    pathArr: Array<string>,
-    tabs: Array<string>
+
+}
+
+interface IPath {
+    name: string,
+    path: string
 }
 
 const HeaderButtons = (props: IProps) => {
+    const path = useContext(PathContext);
+    const appContent = useContext(AppContentContext);
 
-    const ChangePath = (elem: string) => {
-        let pathArr: Array<string> = [...props.pathArr];
-        if (pathArr.length > 1 && elem !== 'KOM') {
+    const ChangePath = (elem: IPath) => {
+        let pathArr: Array<IPath> = [...path.value];
+        if (pathArr.length > 1 && elem.name !== 'KOM') {
             do {
                 pathArr.pop();
             } while (pathArr.length > 1);
             pathArr.push(elem);
-        } else if (elem === 'KOM') {
-            pathArr = ['KOM'];
+        } else if (elem.name === 'KOM') {
+            pathArr = [{name: 'KOM', path: '/home'}];
         } else {
             pathArr.push(elem);
         }
-        props.changePathWay(pathArr);
+        path.changeValue(pathArr);
     }
 
     return(
         <div className="headerTabs-content">
-            {props.tabs.map((elem) =>
-                <div className="headerTab" key={elem}>
-                    <a className="tabName" href="#" onClick={() => ChangePath(elem)}><span>{elem}</span></a>
+            {appContent.value.map((elem) =>
+                <div className="headerTab" key={appContent.value.indexOf(elem)}>
+                    <Link to={elem.link} className="tabName" onClick={() => ChangePath({name: elem.name, path: elem.link})}>{elem.name}</Link>
                 </div>
             )}
         </div>
