42a0de44e51d79b112df4068baba34915a3a1a04
Nikita Tashchilin <tashchilin@proconom.cz>
2020-09-27 16:38:32 +0000

    Updated MainContent.tsx

--- kom_tul/src/components/MainContent/MainContent.tsx
+++ kom_tul/src/components/MainContent/MainContent.tsx
@@ -1,16 +1,25 @@
 import * as React from 'react';
 import StartPage from "./StartPage";
+import {Route, Switch, Redirect} from "react-router-dom";
+import Department from "./Pages/Department";
 
 interface IProps {
-    appLanguage: string;
-    pathWay: Array<string>;
-    changePathWay: (value: Array<string>) => void;
+
 }
 
 const MainContent = (props: IProps) => {
     return (
         <div className="mainContent">
-            <StartPage appLanguage={props.appLanguage}/>
+            <Switch>
+                <Route path='/home' component={StartPage}/>
+                <Route path='/department' render={() => (<Department/>)}/>
+                <Route path='/employees' render={() => (<div></div>)}/>
+                <Route path='/projects' render={() => (<div></div>)}/>
+                <Route path='/education' render={() => (<div></div>)}/>
+                <Route path='/laboratories' render={() => (<div></div>)}/>
+                <Route path='/cooperation' render={() => (<div></div>)}/>
+                <Redirect from='/' to='/home' />
+            </Switch>
         </div>
     );
 }
