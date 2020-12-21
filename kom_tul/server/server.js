701e1ae36502ddf93d9138c417051b7e98fe3f92
aleveha <21022001qQ>
2020-12-21 20:44:01 +0100

    https

--- kom_tul/server/server.js
+++ kom_tul/server/server.js
@@ -1,8 +1,12 @@
 const path = require('path');
+const fs = require('fs');
+const https = require('https');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '../build');
 const port = process.env.PORT || 9000;
+const key = fs.readFileSync('key.pem');
+const cert = fs.readFileSync('cert.pem');
 
 app.use(express.static(publicPath));
 
@@ -10,6 +14,9 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(port, () => {
-    console.log(`Server is up  on port ${port}!!!`);
+https.createServer({
+    key: key,
+    cert: cert
+}, app).listen(port, () => {
+    console.log(`Server is up on https://localhost:${port}`);
 });
