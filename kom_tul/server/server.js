5d340315896f29d4e653c7e0e74ffe9da3478397
aleveha <21022001qQ>
2020-12-22 19:12:12 +0100

    change port to 80

--- kom_tul/server/server.js
+++ kom_tul/server/server.js
@@ -4,7 +4,7 @@ const https = require('https');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '../build');
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 80;
 const key = fs.readFileSync('key.pem');
 const cert = fs.readFileSync('cert.pem');
 
