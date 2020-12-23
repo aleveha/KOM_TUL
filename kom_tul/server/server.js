b4e89e4054cba6989d0f2c9b02236bd7938f08cc
Nikita Tashchilin <tashchilin@proconom.cz>
2020-12-23 12:07:32 +0000

    change port

--- kom_tul/server/server.js
+++ kom_tul/server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '../build');
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 80;
 
 app.use(express.static(publicPath));
 
