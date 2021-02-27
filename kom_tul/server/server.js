91be316db1638a4a39cef28d9e636fbadd9e7aa3
aleveha <nikita.tashilin@gmail.com>
2021-02-27 17:31:26 +0100

    delete unuseful code in server.js

--- kom_tul/server/server.js
+++ kom_tul/server/server.js
@@ -1,12 +1,8 @@
 const path = require('path');
-const fs = require('fs');
-const https = require('https');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '../build');
 const port = process.env.PORT || 80;
-const key = fs.readFileSync('key.pem');
-const cert = fs.readFileSync('cert.pem');
 
 app.use(express.static(publicPath));
 
