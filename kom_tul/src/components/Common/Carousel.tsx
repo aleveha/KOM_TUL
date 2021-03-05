f25e8111c68db26f93eaf43206bf98b9359c1418
aleveha <nikita.tashilin@gmail.com>
2021-03-05 18:40:55 +0100

    add swipeable gallery on start page

---
+++ kom_tul/src/components/Common/Carousel.tsx
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const Carousel = (props: {components: JSX.Element[]}) => {
+    const [index, setIndex] = useState<number>(0);
+    const handleChangeIndex = (index: number) => {
+        setIndex(index);
+    }
+
+    return (
+        <div className="carouselContainer">
+            <AutoPlaySwipeableViews
+                index={index}
+                onChangeIndex={handleChangeIndex}
+                ignoreNativeScroll={true}
+                interval={5000}
+                height="100%"
+            >
+                {props.components.map(comp => comp)}
+            </AutoPlaySwipeableViews>
+        </div>
+    );
+};
+
+export default Carousel;
