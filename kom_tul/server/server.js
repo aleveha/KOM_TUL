01c73900c552656016704ea7ff387ed2069a41fd
aleveha <nikita.tashilin@gmail.com>
2021-03-12 10:15:55 +0100

    add new folder

--- kom_tul/server/server.js
+++ kom_tul/server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '../build');
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8081;
 
 app.use(express.static(publicPath));
 
